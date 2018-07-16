d28d1effa2ee9f6ecc75913a11c436a6b3b7f067
saumya29 <saumyatiwari.29@gmail.com>
2018-07-17 04:48:37 +0530

    [Feat]: Added organisation setup route

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -4,6 +4,8 @@ import authenticate from '../middlewares/authenticate';
 import { getDb } from '../database';
 import { productionConstants } from '../config/constants';
 import usernameExists from '../helpers/usernameExists';
+import getUserID from '../helpers/getUserID';
+import registerOrganisation from '../helpers/handleOrganisation';
 
 const route = express.Router();
 
@@ -40,6 +42,26 @@ export const updateUser = async (db, collection, email, dataToBeUpdated) => {
   }
 };
 
+// export const addOrganisation = async (
+//   db,
+//   usercollection,
+//   organisationCollection,
+//   email,
+//   dataToBeAdded,
+// ) => {
+//   const ownerID = await getUserID(db, usercollection, email);
+//   const { name, description } = dataToBeAdded;
+//   dataToBeAdded['ownerID'] = ownerID,
+//   try {
+
+//   } catch (e) {
+//     return {
+//       errors: {
+//         name: '[Register Org]: Caught an error while creating the organisation',
+//       },
+//     };
+//   }
+// };
 route.get('/', (req, res) => {
   res.json({ success: true });
 });
@@ -56,6 +78,38 @@ route.post('/setup', async (req, res) => {
   const collection = productionConstants.USERS_COLLECTION;
   // update user details
   const response = await updateUser(db, collection, currentUser, req.body);
+  res.json(response);
+});
+
+// For setting up the organisation details
+route.post('/orgSetup', async (req, res) => {
+  /*
+  * name: string
+  * description: dob
+  * OwnerID: string
+  */
+  const { currentUser } = req; // gives email id
+  const db = await getDb();
+  const userCollection = productionConstants.USERS_COLLECTION;
+  const organisationCollection = productionConstants.ORGANISATION_COLLECTION;
+  // update user details
+  const ownerID = await getUserID(db, userCollection, currentUser);
+  if (ownerID === undefined) {
+    res.json({
+      error: {
+        name: 'Could not fetch User ID',
+      },
+    });
+  }
+  console.log(ownerID);
+  const response = await registerOrganisation(
+    db,
+    organisationCollection,
+    {
+      ownerID,
+      ...req.body,
+    },
+  );
   res.json(response);
 });
 
